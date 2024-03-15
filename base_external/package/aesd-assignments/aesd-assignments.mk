6f832d6b6bb1f63c8759a1d37d13ce1c6c73e5c7
Youssef-74 <yosifesam98@gmail.com>
2024-03-15 05:55:40 +0200

    submit assignment-4-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -19,6 +19,7 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# conf directory creation edited
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
